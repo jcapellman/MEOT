e4c480d276d6a7a078b303e68f6e9568ab7f9395
Jarred Capellman <jcapellman@hotmail.com>
2021-03-07 08:52:14 -0600

    Added: Initial commit of the LiteDBDAL class

---
+++ MEOT.lib/DAL/LiteDBDAL.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using MEOT.lib.DAL.Base;
+using MEOT.lib.Objects.Base;
+
+namespace MEOT.lib.DAL
+{
+    public class LiteDBDAL : IDAL
+    {
+        private const string DB_NAME = "meot_litedb.db";
+
+        public void InsertAsync(BaseObject item)
+        {
+            
+        }
+
+        public List<BaseObject> SelectAll<T>() where T : BaseObject
+        {
+            return null;
+        }
+    }
+}
