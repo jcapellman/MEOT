ad94bae337d86bd6d876447843ddba81bfdab65b
Jarred Capellman <jcapellman@hotmail.com>
2021-03-07 13:09:31 -0600

    Added: Initial work on the BaseSource class

---
+++ MEOT.lib/Sources/Base/BaseSource.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MEOT.lib.Sources.Base
+{
+    public abstract class BaseSource
+    {
+        public abstract string Name { get; }
+
+        public abstract bool RequiresKey { get; }
+
+        public abstract void Initialize(string licenseKey);
+
+        public abstract Dictionary<string, bool> QueryHash(string sha1);
+    }
+}
