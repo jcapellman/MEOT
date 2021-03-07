f2e00da0cdfb205d65b89df94331583336caca4f
Jarred Capellman <jcapellman@hotmail.com>
2021-03-07 08:49:51 -0600

    Added: IDAL SingleTon and instantiation of the LiteDBDAL

--- MEOT.Web/Startup.cs
+++ MEOT.Web/Startup.cs
@@ -1,15 +1,12 @@
+using MEOT.lib.DAL;
+using MEOT.lib.DAL.Base;
 using MEOT.Web.Data;
+
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MEOT.Web
 {
@@ -21,17 +18,15 @@ namespace MEOT.Web
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<IDAL>(new LiteDBDAL()); // Using LiteDB
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,7 +36,6 @@ namespace MEOT.Web
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
