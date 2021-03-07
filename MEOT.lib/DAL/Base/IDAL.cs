3d485e579005c385be0276e6a35cffefb717185d
Jarred Capellman <jcapellman@hotmail.com>
2021-03-07 08:51:51 -0600

    Added: Initial commit of the IDAL Interface

---
+++ MEOT.lib/DAL/Base/IDAL.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using MEOT.lib.Objects.Base;
+
+namespace MEOT.lib.DAL.Base
+{
+    public interface IDAL
+    {
+        void InsertAsync(BaseObject item);
+
+        public List<BaseObject> SelectAll<T>() where T : BaseObject;
+    }
+}
