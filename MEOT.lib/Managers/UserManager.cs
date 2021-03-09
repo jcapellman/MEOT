976fb3f0d38210d9c7d1c76949d10144d75542ef
Jarred Capellman <jcapellman@hotmail.com>
2021-03-08 18:52:58 -0600

    Added: UserManager and GetUsers method #3

---
+++ MEOT.lib/Managers/UserManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+using MEOT.lib.DAL.Base;
+using MEOT.lib.Objects;
+
+namespace MEOT.lib.Managers
+{
+    public class UserManager
+    {
+        private readonly IDAL _db;
+
+        public UserManager(IDAL db)
+        {
+            _db = db;
+        }
+
+        public List<User> GetUsers() => _db.SelectAll<User>();
+    }
+}
