79335f871fafc2a96ed0575c433c939e713aadbf
Jarred Capellman <jcapellman@hotmail.com>
2021-03-08 20:28:19 -0600

    Added: ToSHA256 method #1

---
+++ MEOT.lib/Common/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MEOT.lib.Common
+{
+    public static class ExtensionMethods
+    {
+        public static string ToSHA256(this string str)
+        {
+            using var shaManager = new SHA256Managed();
+
+            return BitConverter.ToString(shaManager.ComputeHash(Encoding.UTF8.GetBytes(str))).Replace("-", "");
+        }
+    }
+}
