ee09e4c1510b9a1a10f0aebc323686206734a1b8
Jarred Capellman <jcapellman@hotmail.com>
2021-03-08 18:53:38 -0600

    Added: Initial work on the User class #3

---
+++ MEOT.lib/Objects/User.cs
@@ -0,0 +1,14 @@
+﻿using MEOT.lib.Enums;
+using MEOT.lib.Objects.Base;
+
+namespace MEOT.lib.Objects
+{
+    public class User : BaseObject
+    {
+        public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
+
+        public Roles Role { get; set; }
+    }
+}
