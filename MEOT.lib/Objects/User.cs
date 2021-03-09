ce5a31c7fbc94cf33d256c6b1ba4657cede13c77
Jarred Capellman <jcapellman@hotmail.com>
2021-03-08 19:12:47 -0600

    Added: Defaults and required properties #4

--- MEOT.lib/Objects/User.cs
+++ MEOT.lib/Objects/User.cs
@@ -1,14 +1,27 @@
-﻿using MEOT.lib.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+
+using MEOT.lib.Enums;
 using MEOT.lib.Objects.Base;
 
 namespace MEOT.lib.Objects
 {
     public class User : BaseObject
     {
+        [Required]
         public string EmailAddress { get; set; }
 
+        [Required]
         public string Password { get; set; }
 
         public Roles Role { get; set; }
+
+        public User()
+        {
+            EmailAddress = string.Empty;
+
+            Password = string.Empty;
+
+            Role = Roles.Viewer;
+        }
     }
 }
