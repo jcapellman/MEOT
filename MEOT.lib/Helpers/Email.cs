600c0f03c77124d15d5b5257932e807dd01c18d4
Jarred Capellman <jcapellman@hotmail.com>
2021-03-09 19:45:16 -0600

    Added: Email class #9

---
+++ MEOT.lib/Helpers/Email.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+using MEOT.lib.Objects;
+
+namespace MEOT.lib.Helpers
+{
+    public class Email
+    {
+        private readonly Settings _settings;
+
+        public Email(Settings settings)
+        {
+            _settings = settings;
+        }
+
+        public void Send(string receiver, string subject, string body)
+        {
+            var smtpClient = new SmtpClient(_settings.SMTPAddress)
+            {
+                Port = _settings.SMTPPort,
+                Credentials = new NetworkCredential(_settings.SMTPUser, _settings.SMTPPassword),
+                EnableSsl = true,
+            };
+
+            smtpClient.Send("no-reply@meot", receiver, subject, body);
+        }
+    }
+}
