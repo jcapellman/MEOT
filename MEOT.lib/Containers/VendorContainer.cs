237bccbd16ba65a5f457e36f532451772da3a6ba
Jarred Capellman <jcapellman@hotmail.com>
2021-03-09 19:37:20 -0600

    Added: VendorContainer class #7

---
+++ MEOT.lib/Containers/VendorContainer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MEOT.lib.Containers
+{
+    public class VendorContainer
+    {
+        public int NumDetections { get; set; }
+
+        public int NumAttempts { get; set; }
+
+        public double AverageTimeToDetect { get; set; }
+        
+        public List<VendorDetailedAnalysis> DetailedAnalysis { get; set; }
+    }
+}
