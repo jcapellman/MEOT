160826649e31af669947e32587980bd41a38c30e
Jarred Capellman <jcapellman@hotmail.com>
2021-03-19 20:13:31 -0500

    Added: TrendingAnalysisController #46

---
+++ MEOT.WebAPI/Controllers/TrendingAnalysisController.cs
@@ -0,0 +1,29 @@
+﻿using MEOT.lib.Containers;
+using MEOT.lib.Managers;
+
+using MEOT.WebAPI.Controllers.Base;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MEOT.WebAPI.Controllers
+{
+    public class TrendingAnalysisController : BaseController
+    {
+        private TrendingAnalysisManager _manager;
+
+        public TrendingAnalysisController(TrendingAnalysisManager manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpGet]
+        public TrendingAnalysisContainer Get()
+        {
+            return new()
+            {
+                TopVendors = _manager.GetTopVendorsFromDuration(),
+                WorstVendors = _manager.GetWorstVendorsFromDuration()
+            };
+        }
+    }
+}
