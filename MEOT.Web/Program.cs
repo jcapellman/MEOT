5fd693175a413111a26951c8ca69626ada71ec01
Jarred Capellman <jcapellman@hotmail.com>
2021-03-19 21:05:49 -0500

    Added: Initial work on the Web Assembly port

--- MEOT.Web/Program.cs
+++ MEOT.Web/Program.cs
@@ -1,21 +1,35 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
-namespace MEOT.web
+using MEOT.Web.Clients;
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MEOT.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>()
-                    .UseUrls("http://0.0.0.0:5009", "https://0.0.0.0:5010");
-                });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                // Configure your authentication provider options here.
+                // For more information, see https://aka.ms/blazor-standalone-auth
+                builder.Configuration.Bind("Local", options.ProviderOptions);
+            });
+            
+            builder.Services.AddHttpClient<MalwareDashboardHttpClient>(client =>
+                client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+            await builder.Build().RunAsync();
+        }
     }
 }
---
+++ MEOT.lib/Containers/TrendingAnalysisContainer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MEOT.lib.Containers
+{
+    public class TrendingAnalysisContainer
+    {
+        public List<VendorAnalysis> TopVendors { get; set; }
+
+        public List<VendorAnalysis> WorstVendors { get; set; }
+    }
+}
