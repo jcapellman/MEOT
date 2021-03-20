d611835458f27833c68d76de8cca73d4e2278d98
Jarred Capellman <jcapellman@hotmail.com>
2021-03-19 20:07:54 -0500

    Added: VendorBreakdownController #45

---
+++ MEOT.WebAPI/Controllers/VendorBreakdownController.cs
@@ -0,0 +1,22 @@
+﻿using MEOT.lib.Containers;
+using MEOT.lib.Managers;
+
+using MEOT.WebAPI.Controllers.Base;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MEOT.WebAPI.Controllers
+{
+    public class VendorBreakdownController : BaseController
+    {
+        private MalwareManager _manager;
+
+        public VendorBreakdownController(MalwareManager manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpGet]
+        public VendorContainer Get(string vendorName) => _manager.GetVendorData(vendorName);
+    }
+}
